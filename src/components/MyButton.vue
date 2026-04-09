ea2681eb98033e71f7e203a64f5ad216c53143a7
Mayur Chaudhari <mayur.chaudhari@ranium.in>
2026-04-09 09:55:56 +0530

    Setup Vue project and configured Storybook

---
+++ src/components/MyButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button>{{ label }}</button>
+</template>
+
+<script setup>
+defineProps({
+  label: String
+});
+</script>
---
+++ src/stories/MyButton.stories.js
@@ -0,0 +1,12 @@
+import MyButton from '../components/MyButton.vue';
+
+export default {
+  title: 'Example/MyButton',
+  component: MyButton,
+};
+
+export const Primary = {
+  args: {
+    label: 'Click Me',
+  },
+};
