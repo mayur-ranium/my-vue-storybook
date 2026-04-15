d712692834be7bb6d35bb2f66413f357929a7608
Mayur Chaudhari <mayur.chaudhari@ranium.in>
2026-04-15 15:56:26 +0530

    feat: add TextInput component with customizable properties and Storybook stories for various input configurations

---
+++ src/components/TextInput/TextInput.stories.js
@@ -0,0 +1,59 @@
+import TextInput from "./TextInput.vue";
+
+export default {
+    title: "TextInput",
+    component: TextInput,
+
+    argTypes: {
+        modelValue: { control: "text" },
+        placeholder: { control: "text" },
+        type: { control: "text" },
+        size: {
+            control: "select",
+            options: ["small", "medium", "large"]
+        }
+    }
+}
+
+
+const Template = (args) => ({
+    components: { TextInput },
+    setup() {
+        return { args };
+    },
+    template: '<TextInput v-bind="args" :modelValue="args.modelValue" @update:modelValue="args.modelValue = $event"/>'
+})
+
+export const Default = Template.bind({})
+Default.args = {
+    modelValue: "",
+    placeholder: "Enter default name",
+    size: "medium",
+    type: "text"
+}
+
+export const WithValue = Template.bind({})
+WithValue.args = {
+    modelValue: "Hello Mayur!!!!"
+}
+
+export const Small = Template.bind({})
+Small.args = {
+    placeholder: "Enter small name",
+    size: "small",
+    type: "text"
+}
+
+export const Medium = Template.bind({})
+Medium.args = {
+    placeholder: "Enter name for medium :",
+    size: "medium",
+    type: "text"
+}
+
+export const Large = Template.bind({})
+Large.args = {
+    placeholder: "Enter large name",
+    size: "large",
+    type: "text"
+}
---
+++ src/components/TextInput/TextInput.vue
@@ -0,0 +1,67 @@
+<template>
+   <div class="textInput">
+     <input :type="type" :placeholder="placeholder" :class="sizeClass" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+   </div>
+</template>
+
+<script>
+export default {
+    name: 'TextInput',
+    props: {
+        placeholder : {
+            type: String,
+            default: "Enter name"
+        },
+        type: {
+            type: String,
+            default: "text"
+        },
+        size: {
+            type: String,
+            default: "large"
+        },
+        modelValue: {
+            type: String,
+            default: ''
+        }
+
+    },
+    data() {
+        return {}
+    },
+    computed: {
+        sizeClass() {
+            return {
+                small: this.size === 'small',
+                medium: this.size === 'medium',
+                large: this.size === 'large'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+ .textInput input {
+    border: 1px solid #340505;
+    outline: none;
+    border-radius: 10px;
+ }
+
+ /* size variants */
+.small {
+  padding: 4px;
+  font-size: 12px;
+}
+
+.medium {
+  padding: 6px;
+  font-size: 14px;
+}
+
+.large {
+  padding: 8px;
+  font-size: 16px;
+}
+
+</style>
