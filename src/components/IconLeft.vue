dee07563ee1423511e0cf24b47114781567d43d7
Mayur Chaudhari <mayur.chaudhari@ranium.in>
2026-04-16 10:56:38 +0530

    refactor: remove Card component and its associated Storybook stories

--- src/components/IconLeft.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <svg viewBox="0 0 24 24" fill="currentColor">
-    <path d="M15 18l-6-6 6-6" />
-  </svg>
-</template>
--- src/components/IconRight.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <svg viewBox="0 0 24 24" fill="currentColor">
-    <path d="M9 6l6 6-6 6" />
-  </svg>
-</template>
